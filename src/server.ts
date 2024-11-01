38fb4fa756b44208d1205d587561f619db084484
Saif Maamun <60704362+saifmaamun@users.noreply.github.com>
2024-11-01 11:02:55 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,34 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+// import config from './app/config';
 
-async function server() {
+let server: Server;
+
+async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
   }
 }
-server();
+main();
+
+process.on('unhandledRejection', () => {
+  console.log(`unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
